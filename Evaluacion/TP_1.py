14ae791a9886bba34eb8028b3fda8fafa4b7449f
daxer99 <rodrigo.peralta93@gmail.com>
2023-08-22 19:49:15 -0300

    add Evaluacion folder, tp1

--- Evaluacion/TP_1.py
+++ Evaluacion/TP_1.py
@@ -36,22 +36,53 @@ class Playlist:
             dur +=self.songs[i].get_dur()
         return round(dur,2)
 
-filename = "/media/rodrigo/Datos/Facultad/FIUNER/Fundamentos/AP_OOP/Evaluacion/Spotify 2010 - 2019 Top 100.csv"
-data = read_file(filename)
+filename = "/media/rodrigo/Datos/Facultad/FIUNER/Fundamentos/AP_OOP/Evaluacion/Spotify 2010 - 2021 Top 100.csv"
+# data = read_file(filename)
+#
+# data_songs = []
+# for i in range(len(data)):
+#     data_songs.append(Song(data[i][0],data[i][1],data[i][2],data[i][3],data[i][4]))
+#     print(data_songs[i].get_year())
+#
+# pl_2011 = Playlist("2011","Rodrigo")
+# for i in range(len(data_songs)):
+#     if int(data_songs[i].get_year()) == 2011:
+#         pl_2011.add_song(data_songs[i])
+# print(pl_2011.get_duration())
+#
+# pl_indie = Playlist("Indie","Rodrigo")
+# for i in range(len(data_songs)):
+#     if data_songs[i].get_genre() == "indie rock":
+#         pl_indie.add_song(data_songs[i])
+# print(pl_indie.get_duration())
 
-data_songs = []
-for i in range(len(data)):
-    data_songs.append(Song(data[i][0],data[i][1],data[i][2],data[i][3],data[i][4]))
-    print(data_songs[i].get_year())
+#Cuestionario
+df = pd.read_csv(filename, sep=",")
+#1
+print(1)
+print(df[df["top genre"]=="dance pop"].count())
+#2
+print(2)
+print(df.groupby(['year released'])['dur'].sum().idxmax())
+print(df.groupby(['year released'])['dur'].sum().max())
+#3
+print(3)
+print(df.groupby(['year released'])['dur'].sum().idxmin())
+print(df.groupby(['year released'])['dur'].sum().min())
+#4
+print(4)
+print(df.groupby(['top genre'])['dur'].sum().idxmax())
+print(df.groupby(['top genre'])['dur'].sum().max())
+#5
+print(5)
+print(df.groupby(['top genre'])['dur'].sum().idxmin())
+print(df.groupby(['top genre'])['dur'].sum().min())
+#6
+print(6)
+print(df[df["top genre"]==(df.groupby(['top genre'])['dur'].sum().idxmax())].mean())
+#7
+print(7)
+print(df.groupby(['year released'])['dur'].mean().idxmin())
+print(df.groupby(['year released'])['dur'].mean().min())
 
-pl_2011 = Playlist("2011","Rodrigo")
-for i in range(len(data_songs)):
-    if int(data_songs[i].get_year()) == 2011:
-        pl_2011.add_song(data_songs[i])
-print(pl_2011.get_duration())
 
-pl_indie = Playlist("Indie","Rodrigo")
-for i in range(len(data_songs)):
-    if data_songs[i].get_genre() == "indie rock":
-        pl_indie.add_song(data_songs[i])
-print(pl_indie.get_duration())
